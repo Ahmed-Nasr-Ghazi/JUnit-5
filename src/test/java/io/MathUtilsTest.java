f71a80cde867304af7f50c3d07e8f80267c6a8f5
Ahmed-Nasr-Ghazi <a7med.nasr99@gmail.com>
2022-02-08 22:13:53 +0200

    TDD with JUnit

--- src/test/java/io/MathUtilsTest.java
+++ src/test/java/io/MathUtilsTest.java
@@ -7,11 +7,20 @@ import org.junit.jupiter.api.Test;
 class MathUtilsTest {
 
 	@Test
-	void test() {
+	void testAdd() {
 		MathUtils mathUtils = new MathUtils();
 		int expected = 2;
 		int actual = mathUtils.add(1, 1);
 		assertEquals(expected, actual, "this method should add the two numbers");
 	}
 
+	@Test
+	void testComputeCircleArea() {
+		MathUtils mathUtils = new MathUtils();
+		double expectedArea = mathUtils.computeCircleArea(10);
+		assertEquals(314.1592653589793, expectedArea, "this method should return right circle area");
+	}
+	
+	
 }
+
