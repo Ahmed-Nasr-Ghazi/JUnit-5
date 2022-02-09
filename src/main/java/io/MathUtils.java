c7a052e5269f7ae32ea77e42d88801ef0ab891ec
Ahmed-Nasr-Ghazi <a7med.nasr99@gmail.com>
2022-02-09 19:10:25 +0200

    @BeforeAll hook annotation added

--- src/main/java/io/MathUtils.java
+++ src/main/java/io/MathUtils.java
@@ -1,7 +1,17 @@
 package io;
 
+interface Lam {
+	void execute(int x);
+}
+
+
+
 public class MathUtils {
 
+	public void test(Lam lam, int x) {
+		lam.execute(x);
+	}
+	
 	public int add (int a, int b) {
 		return a + b;
 	}
@@ -20,7 +30,12 @@ public class MathUtils {
 	public static void main(String[] args) {
 		
 		MathUtils mathUtils = new MathUtils();
+
 		
+		Lam lam = (int x) -> System.out.println(x);
+		lam.execute(10);
+		mathUtils.test( lam, 0);
+
 		System.out.println(mathUtils.divide(10, 0));
 		
 	}
--- src/test/java/io/MathUtilsTest.java
+++ src/test/java/io/MathUtilsTest.java
@@ -2,24 +2,27 @@ package io;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MathUtilsTest {
 
-	int cachedValue = 0;
+	MathUtils mathUtils;
+	
+	@BeforeEach
+	void init() {
+		mathUtils = new MathUtils();
+	}
 	
 	@Test
 	void testAdd() {
-		MathUtils mathUtils = new MathUtils();
 		int expected = 2;
-		cachedValue = 10;
 		int actual = mathUtils.add(1, 1);
 		assertEquals(expected, actual, "this method should add the two numbers");
 	}
 
 	@Test
 	void testDevide() {
-		MathUtils mathUtils = new MathUtils();
 		assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0), "divide by zero should throw");
 		/*
 		double expectedResult = mathUtils.divide(10,0);
@@ -29,7 +32,6 @@ class MathUtilsTest {
 	
 	@Test
 	void testComputeCircleArea() {
-		MathUtils mathUtils = new MathUtils();
 		double expectedArea = mathUtils.computeCircleArea(10);
 		assertEquals(314.1592653589793, expectedArea, "this method should return right circle area");
 	}
